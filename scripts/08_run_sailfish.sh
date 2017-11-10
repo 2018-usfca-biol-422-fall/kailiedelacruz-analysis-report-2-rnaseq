51bc349eaf25dd2fe2576ad700d808e7a93bc0dc
Naupaka Zimmerman <naupaka@gmail.com>
2017-11-10 00:34:36 -0800

    Add comments to script 08

--- scripts/08_run_sailfish.sh
+++ scripts/08_run_sailfish.sh
@@ -1,12 +1,21 @@
-# script to run sailfish to count all reads from trimmed reads where both reads (F and R)
-# made it through Trimmomatic QC
+#!/bin/bash
+
+# script to run sailfish to count all reads from trimmed reads where
+# both paired reads (F and R) made it through Trimmomatic QC
 
 # Naupaka Zimmerman
 # November 7, 2017
 
+# Documentation is available here
+# https://arxiv.org/pdf/1308.3700.pdf
+# https://github.com/kingsfordgroup/sailfish
 # see http://sailfish.readthedocs.io/en/master/sailfish.html
 # and http://sailfish.readthedocs.io/en/master/library_type.html#fraglibtype
 # for parameter details
+
+# Also, since sailfish is conceptually very similar to salmon,
+# this guide is also helpful:
+# https://combine-lab.github.io/salmon/getting_started/
 
 # call with bash 08_run_sailfish.sh data/trimmed/*.trim_1P.fastq
 for sample in "$@"
@@ -21,4 +30,3 @@ do
 		-o output/sailfish_quants/$sampleID \
 		-p 88
 done
-
